d3fbd629a3a04cf7edb32a97053a78a7397fde12
Sadegh Majidi <mohammadsadeghmajidiyazdi@hotmail.com>
2021-03-04 12:52:55 +0330

    login api

--- store/urls.py
+++ store/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.views import LoginView
 from django.urls import path
 
 from store.views import index, signup, signup_page
@@ -5,5 +6,6 @@ from store.views import index, signup, signup_page
 urlpatterns = [
     path('', index, name='home_page'),
     path('signup/', signup_page, name='signup_page'),
-    path('signup/submit/', signup, name='signup')
+    path('signup/submit/', signup, name='signup'),
+    path('login', LoginView.as_view(), name='login'),
 ]
