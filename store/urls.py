b3b2bf1f8bf634e5db235954db114ffa6f5b00af
Sadegh Majidi <mohammadsadeghmajidiyazdi@hotmail.com>
2021-03-04 15:52:14 +0330

    fix bu in login

--- store/urls.py
+++ store/urls.py
@@ -1,13 +1,10 @@
-from django.contrib.auth.views import LoginView
 from django.urls import path
 
-from store.views import index, signup, signup_page, login_page, login_user, logout_user
+from store.views import index, signup_page, login_page, logout_user
 
 urlpatterns = [
     path('', index, name='home_page'),
-    path('signup/', signup_page, name='signup_page'),
-    path('signup/submit/', signup, name='signup'),
-    path('login/', login_page, name='login_page'),
-    path('login/submit/', login_user, name='login'),
+    path('signup/', signup_page, name='signup'),
+    path('login/', login_page, name='login'),
     path('logout/', logout_user, name='logout'),
 ]
--- store/views.py
+++ store/views.py
@@ -1,7 +1,6 @@
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
-
 from store.forms import CustomSignUpForm
 
 
@@ -10,21 +9,29 @@ def index(request):
 
 
 def signup_page(request):
-    return render(request=request, template_name='store/register.html', context={})
+    if request.method == 'GET':
+        return render(request=request, template_name='store/register.html', context={})
+    if request.method == 'POST':
+        signup_form = CustomSignUpForm(request.POST)
+        if signup_form.is_valid():
+            signup_form.save()
+        else:
+            return render(request=request, template_name='store/register.html',
+                          context={'error': dict(signup_form.errors.items())['__all__'][0]})
+        return redirect('home_page')
 
 
 def login_page(request):
-    return render(request=request, template_name='store/login.html', context={})
-
-
-def login_user(request):
+    if request.method == 'GET':
+        return render(request=request, template_name='store/login.html', context={})
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
         try:
             user = User.objects.get(username=username)
         except User.DoesNotExist:
-            return render(request=request, template_name='store/login.html', context={'error': 'username is not valid'})
+            return render(request=request, template_name='store/login.html',
+                          context={'error': 'username is not valid'})
         else:
             if not user.check_password(password):
                 return render(request=request, template_name='store/login.html',
@@ -35,17 +42,6 @@ def login_user(request):
             login(request, user)
             print('success')
             return redirect('home_page')
-
-
-def signup(request):
-    if request.method == 'POST':
-        signup_form = CustomSignUpForm(request.POST)
-        if signup_form.is_valid():
-            signup_form.save()
-        else:
-            return render(request=request, template_name='store/register.html',
-                          context={'error': dict(signup_form.errors.items())['__all__'][0]})
-        return redirect('home_page')
 
 
 def logout_user(request):
